1395ccc1d0cfcfe2855b94db1a18336e51d11c3e
Kathick <skarthikeyan25052001@gmail.com>
2024-07-15 18:14:39 +0530

    a

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.png'
 const Logo = () => {
     return (
         <div className='flex flex-row gap-2 items-center cursor-pointer hover:opacity-50'>
-            <img alt='user' src={logo.src} className='hidden sm:flex w-8 h-8 rounded-full bg-neutral-200' />
+            <img alt='user' src={logo.src} className='hidden sm:flex w-8 h-8 rounded-full' />
             <h5 className='text-[18px] font-bold'>Homical</h5>
         </div>
     )
--- app/_components/MobileMenuContent.tsx
+++ app/_components/MobileMenuContent.tsx
@@ -28,7 +28,7 @@ const MobileMenuContent = () => {
             <span className='w-full h-[1px] bg-neutral-200'></span>
             <div className="top flex flex-col gap-2">
                 {Leftbarroutes?.slice(5, 7)?.map((item) => (
-                    <div key={item?.id} className='px-5 h-[50px] flex flex-row items-center gap-5 hover:bg-[var(--blue)] hover:text-white group rounded-[10px] hover:shadow-custom-shadow'>
+                    <div key={item?.id} className='px-5 h-[50px] flex flex-row items-center gap-5 hover:bg-[var(--blue)] hover:text-white group rounded-[10px] hover:shadow-custom-shadow'>                        
                         {item?.icon}
                         <h4>{item?.title}</h4>
                     </div>
